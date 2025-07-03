05b7cfa1d9b4d649d6ef61e6832c5bb73b8c8ba5
Dcarrasc <David.carrascoo@usm.cl>
2025-07-03 18:22:58 -0400

    update test
    Se cambia la lógica de los niveles, para que respete la tabla de niveles de fidelidad y se arregla los % de los beneficios. Se crea lógica para que después de cada compra se actualice el nivel

--- src/main/java/com/fidelidad/Nivel.java
+++ src/main/java/com/fidelidad/Nivel.java
@@ -1,18 +1,35 @@
 package com.fidelidad;
 
 public enum Nivel {
-    BRONCE {
-        public int bono(int base) { return 0; }
-    },
-    PLATA {
-        public int bono(int base) { return (int) (base * 0.05); }
-    },
-    ORO {
-        public int bono(int base) { return (int) (base * 0.10); }
-    },
-    PLATINO {
-        public int bono(int base) { return (int) (base * 0.15); }
-    };
+    BRONCE(0, 499, 0.2),
+    PLATA(500, 1499,0.2),
+    ORO(1500,2999,0.5),
+    PLATINO(3000, Integer.MAX_VALUE, 1.0);
 
-    public abstract int bono(int base);
+    private int minPuntos;
+    private int maxPuntos;
+    private final double bono;
+
+    Nivel(int minPuntos, int maxPuntos, double bono) {
+        this.minPuntos = minPuntos;
+        this.maxPuntos = maxPuntos;
+        this.bono = bono;
+    }
+
+    public double getBono(){
+        return bono;
+    }
+
+    public boolean estaDentroDelRango(int puntos){
+        return puntos >= minPuntos && puntos <= maxPuntos;
+    }
+
+    public static Nivel calcularNivel(int puntosTotales){
+        for (Nivel nivel : values()) {
+            if (nivel.estaDentroDelRango(puntosTotales)) {
+                return nivel;
+            }
+        }
+        return BRONCE;
+    }
 }
