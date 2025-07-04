64ddefefed4e9cf58b55a54257fd98395618d86d
Dcarrasc <David.carrascoo@usm.cl>
2025-07-04 11:46:23 -0400

    Create ClienteServicioTest.java
    Se crea test para implementar las funcionalidades del menú, comenzando por crear un cliente.

---
+++ src/test/java/com/fidelidad/servicio/ClienteServicioTest.java
@@ -0,0 +1,24 @@
+package com.fidelidad.servicio;
+
+import com.fidelidad.Cliente;
+import com.fidelidad.repositorio.ClienteRepositorio;
+import com.fidelidad.repositorio.ClienteRepositorioMemoria;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClienteServicioTest {
+
+    @Test
+    void puedeAgregarClienteValido() {
+        ClienteRepositorio repo = new ClienteRepositorioMemoria();
+        ClienteServicio servicio = new ClienteServicio(repo);
+
+        servicio.crearCliente("1", "David", "david.@mail.com");
+
+        Cliente cliente = repo.buscarPorId("1");
+        assertNotNull(cliente);
+        assertEquals("David", cliente.getNivel());
+        assertEquals("david@mail.com",cliente.getCorreo());
+    }
+}
